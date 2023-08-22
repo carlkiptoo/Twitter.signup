23a02818c2ffa83b7021c3bd5ccd702216239af9
carlkiptoo <carlosiamcarl@gmail.com>
2023-08-23 00:30:51 +0300

    pending problems on form validation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,7 @@
 const fixedInputClass="rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:ring-purple-500 focus:z-10 sm:text-sm"
 
 
+
 export default function Input({
     handleChange,
     value,
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,50 +1,138 @@
-import { useState} from "react";
-import { signupFields} from "../constants/formFields";
+import {useState} from "react";
+import {signupFields} from "../constants/formFields";
 import Input from "./Input";
 import FormAction from "./FormAction";
 
 const fields = signupFields;
-let fieldsState={}
+let fieldsState = {}
 
-fields.forEach(field=> fieldsState[field.id]='');
+fields.forEach(field => fieldsState[field.id] = '');
 
-export default function Signup(){
-    const [signupState, setSignupState]=useState(fieldsState);
+export default function Signup() {
+    const [signupState, setSignupState] = useState(fieldsState);
+    const [errorMessages, setErrorMessages] = useState({});
 
-    const handleChange = (e)=>setSignupState({...signupState, [e.target.id] : e.target.value});
+    const handleChange = (e) => setSignupState({...signupState, [e.target.id]: e.target.value});
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(signupState)
-        createAccount()
+        if (validateForm()) {
+            console.log(signupState)
+            // createAccount()
+        }
+    };
+
+    //FORM VALIDATION
+    function validateForm() {
+
+        const errors = {};
+        //check username
+
+        if (signupState.username === '') {
+            errors.username = 'Username is required';
+        }
+
+
+        //check email
+
+        if (signupState.email === '') {
+            errors.email = 'Email is required';
+        }
+
+        //check password
+
+        if (signupState.password.length < 8) {
+          errors.password = 'Password must contain atleast 8 characters';
+        }
+        //count uppercase
+        let countUpperCase = 0
+        //count lowercase
+        let countLowerCase = 0
+        //count digits
+        let countDigits = 0
+        //count special character
+        let countSpecialCharacters = 0
+
+        for (let i = 0; i < signupState.password.length; i++) {
+            const specialCharacters = [
+                '!',
+                '@',
+                '#',
+                '$',
+                '%',
+                '^',
+                '&',
+                '*',
+                '(',
+                ')',
+                '_',
+                '-',
+                '+',
+                '=',
+                '[',
+                '{',
+                ']',
+                '}',
+                ':',
+                ';',
+                '<',
+                '>',
+            ]
+
+            if (specialCharacters.includes(signupState.password[i])) {
+                countSpecialCharacters++
+            } else if (!isNaN(signupState.password[i] * 1)) {
+                countDigits++
+            } else {
+                if (signupState.password[i] == signupState.password[i].toUpperCase()) {
+                    countUpperCase++
+                }
+                if (signupState.password[i] === signupState.password[i].toLowerCase()) {
+                    countLowerCase++
+                }
+            }
+        }
+        if (countLowerCase === 0) {
+           errors.passwordLowerCase = 'Password must contain at least one lowercase letter';
+        }
+        if (countUpperCase === 0) {
+           errors.passwordUpperCase = 'Password must contain at least one uppercase letter';
+        }
+        if (countDigits === 0) {
+            errors.passwordDigits = 'Password must contain at least one number'
+        }
+
+
+        setErrorMessages(errors);
+
+        return Object.keys(errors).length === 0
+;
     }
 
-    //API
-    const createAccount=()=>{
-
-    }
-
-    return(
+    return (
         <form className='mt-8 space-y-6' onSubmit={handleSubmit}>
             <div className=''>
                 {
-                    fields.map(field=>
-                    <Input
-                        key={field.id}
-                        handleChange={handleChange}
-                        value={signupState[field.id]}
-                        labelText={field.labelText}
-                        labelFor={field.labelFor}
-                        id={field.id}
-                        name={field.name}
-                        type={field.type}
-                        isRequired={field.isRequired}
-                        placeholder={field.placeholder}
+                    fields.map(field => (
+                        <div key={field.id}>
+                        <Input
+                            key={field.id}
+                            handleChange={handleChange}
+                            value={signupState[field.id]}
+                            labelText={field.labelText}
+                            labelFor={field.labelFor}
+                            id={field.id}
+                            name={field.name}
+                            type={field.type}
+                            isRequired={field.isRequired}
+                            placeholder={field.placeholder}
                         />
+                            {errorMessages[field.id] && <p className="text-red-500">{errorMessages[field.id]}</p>}
 
-                    )
+                        </div>
+                    ))
                 }
-                <FormAction handleSubmit={handleSubmit} text='Signup' />
+                <FormAction handleSubmit={handleSubmit} text='Signup'/>
             </div>
         </form>
     )
